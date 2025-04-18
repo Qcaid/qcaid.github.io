0e8dbafe116099294ffed7228e6a05e730e88f2b
xyu <2152256010@qq.com>
2025-04-19 01:25:25 +0800

    Site updated: 2025-04-19 01:25:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/16/hello-world/","2025/04/18/reak/","2025/04/18/sqz/","2025/04/18/gbjd/"];function toRandomPost(){
+var posts=["2025/04/16/hello-world/","2025/04/18/reak/","2025/04/18/sqz/","2025/04/18/gbjd/","2025/04/19/hello-dev-world/","2025/04/19/mtspark/","2025/04/19/funvsc/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
