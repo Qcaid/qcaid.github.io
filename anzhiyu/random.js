fe2780041570d3caff3bfe70a0ada14c113a216b
xyu <2152256010@qq.com>
2025-04-18 21:26:57 +0800

    Site updated: 2025-04-18 21:26:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/16/hello-world/","2025/04/18/reak/","2025/04/18/sqz/"];function toRandomPost(){
+var posts=["2025/04/16/hello-world/","2025/04/18/reak/","2025/04/18/sqz/","2025/04/18/gbjd/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
